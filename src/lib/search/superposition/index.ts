8b9e41e69f0fc086f88fc9b148e7a9ab5c661663
MantisAI <mantis.bot.ai@gmail.com>
2026-05-18 20:40:59 +0300

    chore(search): add superposition module barrel

---
+++ src/lib/search/superposition/index.ts
@@ -0,0 +1,4 @@
+export * from './ConflictComponentDiagnostics.js';
+export * from './SuperpositionChoice.js';
+export * from './SuperpositionPotentialDiagnostics.js';
+export * from './SuperpositionSearchGraph.js';
